e904f18d8a171bb2c104aadca4dfd9204ba103ac
Bora <gundogubora24@gmail.com>
2024-02-04 21:56:15 +0300

    Initial Commit

---
+++ DailyCosmos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyCosmos
+//
+//  Created by Bora Gündoğu on 4.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyCosmos/DailyCosmosApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyCosmosApp.swift
+//  DailyCosmos
+//
+//  Created by Bora Gündoğu on 4.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DailyCosmosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
