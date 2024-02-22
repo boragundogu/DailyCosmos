ef9343d540c85b9719977e031bda0b253d5d293a
Bora <gundogubora24@gmail.com>
2024-02-22 22:55:16 +0300

    added detailView, updated MainViewIMG

---
+++ DailyCosmos/View/IMGDetail/Detail.swift
@@ -0,0 +1,18 @@
+//
+//  Detail.swift
+//  DailyCosmos
+//
+//  Created by Bora Gündoğu on 22.02.2024.
+//
+
+import SwiftUI
+
+struct Detail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Detail()
+}
